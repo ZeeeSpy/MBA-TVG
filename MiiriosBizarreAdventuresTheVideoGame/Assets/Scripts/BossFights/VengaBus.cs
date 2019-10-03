6c7256867e753e496125bd67b8a1d0b05c43fab0
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-03 11:56:09 +0100

    VengaBus scripting done
    Core of the Vengabus boss fight is done, still needs to damage player and add more obsticles for difficulty

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/Scripts/BossFights/VengaBus.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VengaBus : MonoBehaviour, Shootable
+{
+    public Transform player;
+    private Rigidbody thisbus;
+    private bool FindingPlayer = false;
+    private int force = 9000;
+    private int HP = 200;
+    //Max Hp 200
+    private int phase = 1;
+    public Slider Hpbar;
+    bool charging = false;
+
+    private void Start()
+    {
+        thisbus = gameObject.GetComponent<Rigidbody>();
+        StartCoroutine("BossLoop");
+
+    }
+
+    void Update()
+    {
+        if (FindingPlayer)
+        {
+            Vector3 targetPostition = new Vector3(player.position.x, this.transform.position.y, player.position.z);
+            //this.transform.LookAt(targetPostition);
+
+            Vector3 relativePos = targetPostition - transform.position;
+            Quaternion toRotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 5 * Time.deltaTime);
+        }
+    }
+
+    IEnumerator BossLoop()
+    {
+        while (true)
+        {
+            FindingPlayer = true;
+            yield return new WaitForSeconds(3f);
+            FindingPlayer = false;
+            charge();
+            yield return new WaitForSeconds(3f);
+            charging = false;
+        }
+    }
+
+    private void charge() //Needed othwerwise the coroutine breaks and it charges multiple times per charge
+    {
+        if (!charging)
+        {
+            thisbus.AddForce(transform.forward * force * phase);
+            charging = true;
+        }
+    }
+
+    private void Phase2()
+    {
+        phase = 2;
+        Debug.Log("Phase 2");
+        //Add saws?
+    }
+
+    public void GetShot()
+    {
+        HP = HP - 1;
+        Hpbar.value = HP;
+
+        if (HP < 100)
+        {
+            Phase2();
+        }
+    }
+}
+

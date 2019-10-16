cec76a7cd5fe40d92c6294d46f6aeb2cf0090f97
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-16 12:32:49 +0100

    Getaway Car Left/Right movement with clamp

--- MiiriosBizarreAdventuresTheVideoGame/Assets/GoForward.cs
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/GoForward.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class GoForward : MonoBehaviour
 {
     private readonly int speed = 20;
+    private readonly float leftmostlimit = 4.3f;
+    private readonly float rightmostlismit = -4.3f;
+    private readonly float turnspeed = 0.4f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        Vector3 direction = new Vector3(0,0,1);
+
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            if (!(transform.position.x < rightmostlismit))
+            {
+                direction = new Vector3(direction.x - turnspeed, direction.y, direction.z);
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") < 0) //Go left relative to looking back
+        {
+            if (!(transform.position.x > leftmostlimit))
+            {
+                direction = new Vector3(direction.x + turnspeed, direction.y, direction.z);
+            }
+        }
+
+
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }

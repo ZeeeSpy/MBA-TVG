9b46f85052e03926a763e6f0902b7021f9c0f64e
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-17 16:19:11 +0100

    Heli sway

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/GoForwardSimple.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoForwardSimple : MonoBehaviour
+{
+    private Vector3 direction = new Vector3(0, 0, 1);
+    private void FixedUpdate()
+    {
+        transform.Translate(direction * 20 * Time.deltaTime);
+    }
+}
---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/HeliCopterSway.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliCopterSway : MonoBehaviour
+{
+    
+    private readonly float smooth = 1f;
+    Quaternion maxy = Quaternion.Euler(0, 220f, 0);
+    Quaternion miny = Quaternion.Euler(0, 150f, 0);
+    bool swaymax = true;
+
+    private void Start()
+    {
+        StartCoroutine("PanLeftRight");
+    }
+
+    IEnumerator PanLeftRight()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            swaymax = !swaymax;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (swaymax)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, maxy, (smooth * Time.deltaTime) * 3);
+        } else
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, miny, (smooth * Time.deltaTime) * 3);
+        }
+    }
+}

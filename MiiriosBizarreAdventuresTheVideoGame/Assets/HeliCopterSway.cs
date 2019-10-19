a8f356a74a4cf8219806fe9a96661bbc06b1f532
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-19 15:29:33 +0100

    Heli Sway
    Sway reflects direction helicopter is moving

--- MiiriosBizarreAdventuresTheVideoGame/Assets/HeliCopterSway.cs
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/HeliCopterSway.cs
@@ -6,32 +6,29 @@ public class HeliCopterSway : MonoBehaviour
 {
     
     private readonly float smooth = 1f;
-    Quaternion maxy = Quaternion.Euler(0, 220f, 0);
-    Quaternion miny = Quaternion.Euler(0, 150f, 0);
-    bool swaymax = true;
-
-    private void Start()
-    {
-        StartCoroutine("PanLeftRight");
-    }
-
-    IEnumerator PanLeftRight()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(3f);
-            swaymax = !swaymax;
-        }
-    }
+    Quaternion maxy = Quaternion.Euler(0, 220f, 22);
+    Quaternion miny = Quaternion.Euler(0, 150f, -22);
+    bool swaydirection = false;
+    //false = sway left relative to helicopter;
 
     private void FixedUpdate()
     {
-        if (swaymax)
+        if (swaydirection)
         {
             transform.localRotation = Quaternion.Slerp(transform.localRotation, maxy, (smooth * Time.deltaTime) * 3);
         } else
         {
             transform.localRotation = Quaternion.Slerp(transform.localRotation, miny, (smooth * Time.deltaTime) * 3);
         }
+    }
+
+    public void toggledirection()
+    {
+        swaydirection = !swaydirection;
+    }
+
+    public void toggledirection(bool direction)
+    {
+        swaydirection = direction;
     }
 }
---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/LeftRight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRight : MonoBehaviour
+{
+    private bool sway = false;
+    //sway false = left relative to helicopter
+    private bool pause = false;
+    public HeliCopterSway thissway;
+
+    void Start()
+    {
+        StartCoroutine("GoLeftRight");
+    }
+
+    IEnumerator GoLeftRight()
+    {
+        while (true)
+        {
+            if (transform.position.x > 16) //Gone too far right
+            {
+                sway = false;
+                thissway.toggledirection(false);
+            }
+            else if (transform.position.x < -16)
+            {
+                sway = false;
+                thissway.toggledirection(false);
+            }
+            else
+            {
+                sway = !sway;
+                thissway.toggledirection();
+            }
+
+            yield return new WaitForSeconds(Random.Range(1, 6));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (sway)
+        {
+            transform.Translate(new Vector3(1,0,0) * 5 * Time.deltaTime);
+        } else
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * 5 * Time.deltaTime);
+        }
+    }
+}

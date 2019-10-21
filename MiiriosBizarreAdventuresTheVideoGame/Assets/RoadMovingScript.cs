5269230ebb15fab46c21873a50a0dd13913c34eb
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-21 17:05:22 +0100

    Change relative movmenet
    roadmoves and everything is stationary as opposed to everything moving and road is stationary

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/RoadMovingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMovingScript : MonoBehaviour
+{
+    private Vector3 direction = new Vector3(0, 0, -1);
+
+
+    void Start()
+    {
+        StartCoroutine("DestroySelf");
+    }
+
+    
+    private void FixedUpdate()
+    {
+        transform.Translate(direction * 40 * Time.deltaTime);
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(30f);
+        Destroy(gameObject);
+    }
+}
--- MiiriosBizarreAdventuresTheVideoGame/Assets/Scripts/Level 3/RoadSpawningScript.cs
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/Scripts/Level 3/RoadSpawningScript.cs
@@ -4,34 +4,20 @@ using UnityEngine;
 
 public class RoadSpawningScript : MonoBehaviour
 {
-    public Transform player;
     public GameObject road;
-    private int roadnumb = 0;
-    private readonly int roadspawnlimit = 10;
-    private GameObject[] currentroads = new GameObject[10];
-    public GameObject startingroad;
 
     private void Start()
     {
-        StartCoroutine("DestroyRoads");
+        StartCoroutine("SpawnRoad");
     }
 
-    void Update()
+
+    IEnumerator SpawnRoad()
     {
-        if (player.position.z > (roadnumb * 100))
+        while (true)
         {
-            roadnumb++;
-            if (currentroads[roadnumb % roadspawnlimit] != null)
-            {
-                Destroy(currentroads[roadnumb % roadspawnlimit]);
-            }
-            currentroads[roadnumb % roadspawnlimit] = Instantiate(road, transform.position + new Vector3(0, 0, ((100 * roadnumb)+400)), transform.rotation);
+            GameObject SpawnedRoad = Instantiate(road, transform.position + new Vector3(0, 0.5f, 0), transform.rotation);
+            yield return new WaitForSeconds(2.4f);
         }
-    }
-
-    IEnumerator DestroyRoads()
-    {
-        yield return new WaitForSeconds(1f);
-        Destroy(startingroad);
     }
 }

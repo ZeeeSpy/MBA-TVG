8350bf06afc126765118d5da1a8b203aacf3eb79
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-23 23:56:43 +0100

    Started work on credits

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/TextScrollScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScrollScript : MonoBehaviour
+{
+    private void Start()
+    {
+        TextAsset mytxtData = (TextAsset)Resources.Load("CreditsText");
+        GetComponent<Text>().text = mytxtData.text;
+    }
+    void Update()
+    {
+        transform.Translate(0, 1, 0);  
+    }
+}

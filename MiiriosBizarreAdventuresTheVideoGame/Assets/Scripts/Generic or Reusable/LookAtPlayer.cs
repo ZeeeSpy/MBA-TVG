aa446d4a41af648cf798f1a61f486399f82018fe
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-10-24 16:57:00 +0100

    Xboxgang interact

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/Scripts/Generic or Reusable/LookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    private Transform playerlocation;
+
+    void Start()
+    {
+        playerlocation = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(playerlocation);
+    }
+    
+}

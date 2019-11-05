48dcd1e3688e6e258cd9eb2b6add094e6c4914ca
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-11-05 14:56:15 +0000

    Padoru Boss Fight Done

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/SkipSong.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipSong : MonoBehaviour, Shootable
+{
+    public GameObject todisable;
+    public AudioSource globalmusic;
+    public AudioClip musictoplay;
+    public GameObject padoruHPbar;
+    private bool activated = false;
+
+    public void GetShot()
+    {
+        todisable.SetActive(false);
+        padoruHPbar.SetActive(false);
+        if (!activated)
+        {
+            globalmusic.Stop();
+            globalmusic.clip = musictoplay;
+            globalmusic.Play();
+            activated = true;
+        }
+    }
+}

1b565789af2b09231a899a1ecabaf03fd89629e7
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-11-09 20:21:48 +0000

    5B stuff

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/DemoBarHurtScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoBarHurtScript : MonoBehaviour
+{
+    public int DamageVal = 3;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerScript>().Damage(DamageVal);
+        }
+    }
+}

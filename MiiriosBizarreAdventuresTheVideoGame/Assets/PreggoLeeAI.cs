e5c5abd503184cf2bc8029c7718cb03956be5bbd
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-11-12 13:44:56 +0000

    Level 5 B Start

---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/PreggoLeeAI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreggoLeeAI : MonoBehaviour
+{
+    public Level1TextStuff texttoscreen;
+    public Sprite PreggoLeeSprite;
+
+    public GameObject Gun;
+    public GameObject Crosshair;
+    public GameObject BulletSpawner;
+    public GameObject Spinner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("LeeIntroMonologue");
+    }
+
+
+    IEnumerator LeeIntroMonologue()
+    {
+        yield return new WaitForSeconds(0);
+        texttoscreen.showtext("Been waiting long?");
+
+        yield return new WaitForSeconds(2);
+        texttoscreen.showtext("Why do you hate me?");
+
+        yield return new WaitForSeconds(2);
+        texttoscreen.showtext("Why do you pick me if you hate me?");
+
+        yield return new WaitForSeconds(3);
+        texttoscreen.showtext("Even when we win you tell me to shut up");
+
+        yield return new WaitForSeconds(4);
+        texttoscreen.showtext("Worse still, you defiled me. You turned me into THIS. I won't give you a chance, prepare to die");
+
+        Gun.SetActive(false);
+        Crosshair.SetActive(false);
+        BulletSpawner.SetActive(false);
+
+        yield return new WaitForSeconds(5);
+        texttoscreen.showtext("");
+        GetComponent<SpriteRenderer>().sprite = PreggoLeeSprite;
+        transform.localScale = new Vector3(10, 10, 1);
+        yield return new WaitForSeconds(2);
+        Spinner.SetActive(true);
+        StartCoroutine("Survive");
+    }
+
+    IEnumerator SurviveTimer()
+    {
+    yield return new WaitForSeconds(60);
+    }
+
+    IEnumerator Survive()
+    {
+        Coroutine timer = StartCoroutine(SurviveTimer());
+        while (true)
+        {
+            //survive loop
+        }
+    }
+}
---
+++ MiiriosBizarreAdventuresTheVideoGame/Assets/StartFinalBossScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFinalBossScript : MonoBehaviour
+{
+    public GameObject Boss;
+    public GameObject Walkwavy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine("FinalBossCutScene");
+        }
+    }
+
+    IEnumerator FinalBossCutScene()
+    {
+        Walkwavy.SetActive(false);
+        yield return new WaitForSeconds(2.5f);
+        Boss.SetActive(true);
+    }
+}
